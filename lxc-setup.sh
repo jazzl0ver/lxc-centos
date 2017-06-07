58037a0606972995b57c13f41a5560a672b4f11d
jazzl0ver <jazzl0ver@users.noreply.github.com>
2017-06-07 15:29:35 +0300

    Update lxc-setup.sh

--- lxc-setup.sh
+++ lxc-setup.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-[ -e /etc/init.d/lxc-halt ] || (
-wget https://raw.githubusercontent.com/jazzl0ver/lxc-centos/master/lxc-halt -O /etc/init.d/lxc-halt
-ln -s ../init.d/lxc-halt /etc/rc.d/rc0.d/S00lxc-halt
-ln -s ../init.d/lxc-halt /etc/rc.d/rc6.d/S00lxc-reboot
-)
+[ -e /etc/init.d/lxc-halt ] || wget https://raw.githubusercontent.com/jazzl0ver/lxc-centos/master/lxc-halt -O /etc/init.d/lxc-halt
+[ -e /etc/rc.d/rc0.d/S00lxc-halt ] || ln -s ../init.d/lxc-halt /etc/rc.d/rc0.d/S00lxc-halt
+[ -e /etc/rc.d/rc6.d/S00lxc-reboot ] || ln -s ../init.d/lxc-halt /etc/rc.d/rc6.d/S00lxc-reboot
 
-[ -e /etc/init/lxc-sysinit.conf ] || (
-wget https://raw.githubusercontent.com/jazzl0ver/lxc-centos/master/lxc-sysinit.conf -O /etc/init/lxc-sysinit.conf
-)
-[ -e /etc/init/power-status-changed.conf ] || (
-wget https://raw.githubusercontent.com/jazzl0ver/lxc-centos/master/power-status-changed.conf -O /etc/init/power-status-changed.conf
-)
+[ -e /etc/init/lxc-sysinit.conf ] || wget https://raw.githubusercontent.com/jazzl0ver/lxc-centos/master/lxc-sysinit.conf -O /etc/init/lxc-sysinit.conf
+[ -e /etc/init/power-status-changed.conf ] || wget https://raw.githubusercontent.com/jazzl0ver/lxc-centos/master/power-status-changed.conf -O /etc/init/power-status-changed.conf
 
 grep -q lxc /etc/securetty || (
 /bin/cp /etc/securetty /etc/securetty.bak
